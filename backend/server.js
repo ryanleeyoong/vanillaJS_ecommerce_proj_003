f632d6358ac3611f0e26537c8905836e7a49fe76
Ryan Lee <shapiferoaf@gmail.com>
2023-12-26 13:06:16 +0800

    setup Api for specific product data

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,18 @@ app.use(cors());
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+// End-point to get information about a specific product
+app.get('/api/products/:id', (req, res) => {
+  // req.params.id is what the user enters for id on url
+  /* use find() on data.products array and execute a function for each 
+  element in the array. */
+  const product = data.products.find((x)=> x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found!' });
+  }
+})
 
 app.listen(5001, () => {
   console.log('serve at http://localhost:5001');
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,15 @@
+import { getProduct } from '../api';
+import { parseRequestURL } from '../utils';
+
 const ProductScreen = {
-  render: () => '<div>ProductScreen</div>',
+  render: async () => {
+    const request = parseRequestURL();
+    const product = await getProduct(request.id);
+    if(product.error){
+      return(`<div>${product.error}</div>`);
+    }
+    return `<h1>${product.name}</h1>`;
+
+  },
 };
 export default ProductScreen;
