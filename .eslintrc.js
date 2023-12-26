cf84e75ac3db6bbe5c21a84fed6818715b892933
Ryan Lee <shapiferoaf@gmail.com>
2023-12-26 10:48:48 +0800

    Added rating component

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,14 @@ module.exports = {
     node: true,
     es2020: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 11,
   },
   rules: {
     'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-nested-ternary': 0,
   },
 };
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Rating from '../components/Rating';
 // import data from "../data.js";
 const HomeScreen = {
   render: async () => {
@@ -30,6 +31,12 @@ const HomeScreen = {
                             <a href="/#/product/${product._id}">
                                 ${product.name}
                             </a>
+                        </div>
+                        <div class="product-rating">
+                            ${Rating.render({
+                                value: product.rating, 
+                                text: `${product.numReviews} reviews`,
+                            })}
                         </div>
                         <div class="product-brand">
                             ${product.brand}
