039f017e891b368892058139011ba8bc020e86fb
Ryan Lee <shapiferoaf@gmail.com>
2023-12-26 19:08:39 +0800

    Improved UI for product details page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { getProduct } from '../api';
 import { parseRequestURL } from '../utils';
+import Rating from '../components/Rating'
 
 const ProductScreen = {
   render: async () => {
@@ -8,7 +9,55 @@ const ProductScreen = {
     if(product.error){
       return(`<div>${product.error}</div>`);
     }
-    return `<h1>${product.name}</h1>`;
+    return `
+      <div class="content">
+        <div class="back-to-result">
+          <a href="/#/">Back to result</a>
+        </div>
+        <div class="details">
+          <div class="details-image">
+            <img src="${product.image}" alt="${product.name}" />
+          </div>
+          <div class="details-info">
+            <ul>
+              <li>
+                <h1>${product.name}</h1>
+              </li>
+              <li>
+                ${Rating.render({
+                  value: product.rating, 
+                  text: `${product.numReviews} reviews`,
+                })}
+              </li>
+              <li>
+                Price: <strong>$${product.price}</strong>
+              </li>
+              <li>
+                Description:
+                <div>
+                  ${product.description}
+                </div>
+              </li>
+          </div>
+        <div class="details-action">
+          <ul>
+            <li>
+              Price: $${product.price}
+            </li>
+            <li>
+              Status: 
+                ${product.countInStock > 0 
+                  ? `<span class="success">In stock</span>` 
+                  : `<span class="error">Unavailable</span>`
+                }
+            </li>
+            <li>
+                <button id="add-button" class="fw primary">Add to Cart</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    `;
 
   },
 };
